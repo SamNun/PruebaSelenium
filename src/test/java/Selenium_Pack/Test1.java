18880f3b3967ead76c0e81117c12b6a6823801ee
Samuel Nuñez <samuel.nunez1@utp.ac.pa>
2022-10-23 19:43:36 -0500

    Se crean cambios en el test con Selenium

--- src/test/java/Selenium_Pack/Test1.java
+++ src/test/java/Selenium_Pack/Test1.java
@@ -1,6 +1,8 @@
 package Selenium_Pack;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
@@ -9,7 +11,10 @@ public static void main (String[] args)throws InterruptedException {
 	System.setProperty("webdriver.chrome.driver","./src/test/resources/ChromeDriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver(); 
 		driver.get("https://www.amazon.com/");
-		//inserta tu código aquí 
+		WebElement myElement = driver.findElement(By.id("nav-link-accountList-nav-line-1"));
+		myElement.click();
+		WebElement correo = driver.findElement(By.name("email"));
+		correo.sendKeys("tutorial");
 		Thread.sleep(5000);
 		driver.quit();
 	}
